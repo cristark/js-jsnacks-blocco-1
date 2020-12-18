c932e0eee99e66c18d31ac13701d52e24f557999
Cristiano <cristiano.tarconi@gmail.com>
2020-12-18 10:23:23 +0100

    comment on log + error correction

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ somma = 0;
 number = [];
 
 // Variabile con FOR
-for (i = 0; i <= 5; i++) {
+for (i = 0; i < 5; i++) {
     number.push(parseFloat(prompt('Inserisci un numero:')));
     somma += number[i]; 
 }
 
-console.log(number);
-console.log(somma);
+console.log('I numeri che hai inserito sono: ' + number);
+console.log('La somma dei numeri inseriti é: ' + somma);
 
 //VARIABILE CON WHILE
 var numWhile, sumWhile;
@@ -21,10 +21,10 @@ var numWhile, sumWhile;
 var i = 0;
 sumWhile = 0;
 
-while (i <= 5) {
+while (i < 5) {
     numWhile = parseFloat(prompt('Inserisci un numero:'));
     i++;
     sumWhile += numWhile;
 }
 
-console.log(sumWhile);
+console.log('La somma dei numeri inseriti é: ' + sumWhile);
