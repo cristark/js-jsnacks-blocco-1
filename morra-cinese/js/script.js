0c8d1d123d7166d7137def1eaba3d6f4dfa85e56
Cristiano <cristiano.tarconi@gmail.com>
2020-12-26 17:27:46 +0100

    morra cinese js logic with scores

--- morra-cinese/js/script.js
+++ morra-cinese/js/script.js
@@ -1,53 +1,78 @@
-// | GIOCO CARTA FORBICE SASSO
+// | GIOCO CARTA FORBICE SASSO - MORRA CINESE
 console.log('--------------');
 console.log('MORRA CINESE');
 console.log('--------------');
 
 // Assegno array base gioco
 var rspArr = ['carta', 'forbice', 'sasso'];
+// Assegno variabili punteggi base per i giocatori
+var userScore = 0;
+var cpuScore = 0;
 
-// Creazione prompt per decisione segno UTENTE
-var rspUserChoice = prompt('Scegli tra carta, forbice o sasso');
-console.log(rspUserChoice);
+// Variabile sentinella per chiudere il ciclo while al raggiungimento del punteggio max
+var sentinel = true;
 
-// Assegnazione random segno CPU
-var rspCpuNumChoice = Math.floor(Math.random() * 3);
-console.log(rspCpuNumChoice);
-var rspCpuChoice = rspArr[rspCpuNumChoice];
-console.log(rspCpuChoice);
+while (sentinel) {
+    // Creazione prompt per decisione segno UTENTE
+    var rspUserChoice = prompt('Scegli tra carta, forbice o sasso');
+    console.log('UTENTE sceglie: ' + rspUserChoice);
 
-//Variabile esito partita
-var rspResult;
+    // Assegnazione random segno CPU
+    var rspCpuNumChoice = Math.floor(Math.random() * 3);
+    var rspCpuChoice = rspArr[rspCpuNumChoice];
+    console.log('CPU sceglie: ' + rspCpuChoice);
 
-if (rspUserChoice == 'carta' && rspCpuChoice == 'forbice') {
-    rspResult = rspArr[1];
-}   else if (rspUserChoice == 'carta' && rspCpuChoice == 'sasso') {
-    rspResult = rspArr[0];
-}   else if (rspUserChoice == 'forbice' && rspCpuChoice == 'sasso') {
-    rspResult = rspArr[2];
-}   else if (rspUserChoice == 'forbice' && rspCpuChoice == 'carta') {
-    rspResult = rspArr[1];
-}   else if (rspUserChoice == 'sasso' && rspCpuChoice == 'carta') {
-    rspResult = rspArr[0];
-}   else if (rspUserChoice == 'sasso' && rspCpuChoice == 'forbice') {
-    rspResult = rspArr[2];
-}   else  {
-    rspResult = 'Parità!';
+    //Variabile esito partita
+    var rspResult;
+
+    if (rspUserChoice == 'carta' && rspCpuChoice == 'forbice') {
+        rspResult = rspArr[1];
+    }   else if (rspUserChoice == 'carta' && rspCpuChoice == 'sasso') {
+        rspResult = rspArr[0];
+    }   else if (rspUserChoice == 'forbice' && rspCpuChoice == 'sasso') {
+        rspResult = rspArr[2];
+    }   else if (rspUserChoice == 'forbice' && rspCpuChoice == 'carta') {
+        rspResult = rspArr[1];
+    }   else if (rspUserChoice == 'sasso' && rspCpuChoice == 'carta') {
+        rspResult = rspArr[0];
+    }   else if (rspUserChoice == 'sasso' && rspCpuChoice == 'forbice') {
+        rspResult = rspArr[2];
+    }   else  {
+        rspResult = 'Parità!';
+    }
+
+    console.log('VINCE: ' + rspResult);
+
+    // Variabile per capire chi è il vincitore della partita
+    var rspMessagge;
+
+    if (rspResult == rspUserChoice) {
+        rspMessagge = 'HAI VINTO!';
+        userScore += 1;
+    }   else if (rspResult == rspCpuChoice) {
+        rspMessagge = 'HAI PERSO!';
+        cpuScore += 1;
+    }   else {
+        rspMessagge = 'Parità';
+    }
+
+    console.log(rspMessagge);
+    console.log('Utente: ' + userScore);
+    console.log('CPU: ' + cpuScore);
+    alert(rspMessagge);
+    alert('Utente: ' + userScore + '\n' + 'CPU: ' + cpuScore);
+
+    if (userScore == 4 || cpuScore == 4) {
+        sentinel = false;
+    }
 }
 
-console.log(rspResult);
-
-// Variabile per capire chi è il vincitore della partita
-var rspMessagge;
-
-if (rspResult == rspUserChoice) {
-    rspMessagge = 'HAI VINTO!';
-}   else if (rspResult == rspCpuChoice) {
-    rspMessagge = 'HAI PERSO!';
+if (userScore == 4) {
+    rspMessagge = 'COMPLIMENTI! HAI VINTO LA PARTITA!';
 }   else {
-    rspMessagge = 'Parità';
-}
+    rspMessagge = 'Mi dispiace, hai perso la partita. Tenta di nuovo!';
+}   
 
-console.log(rspMessagge);
+alert(rspMessagge);
 
 
