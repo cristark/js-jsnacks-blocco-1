e078eddbe45306b7033369f802858252a551b155
Cristiano <cristiano.tarconi@gmail.com>
2020-12-28 16:55:38 +0100

    game running in html + reset button

--- morra-cinese/js/graph_script.js
+++ morra-cinese/js/graph_script.js
@@ -16,6 +16,8 @@ var rspMessagge;
 var username = prompt('Inserisci il tuo nome di gioco:');
 document.getElementById('user_name').innerHTML = username;
 
+
+// | SASSO 
 var rockButton = document.getElementById("rock");
 rockButton.addEventListener("click", function() {
 
@@ -34,27 +36,119 @@ rockButton.addEventListener("click", function() {
     // Confronto risultato partita con scelta utente
     if (rspResult == rspUserChoice) {
         rspMessagge = 'HAI VINTO!';
-        document.getElementById('final_result').innerHTML = rspMessagge;
         userScore += 1;
         document.getElementById('score_user').innerHTML = userScore;
     }   else if (rspResult == rspCpuChoice) {
         rspMessagge = 'HAI PERSO!';
-        document.getElementById('final_result').innerHTML = rspMessagge;
         cpuScore += 1;
         document.getElementById('score_cpu').innerHTML = cpuScore;
     }   else {
         rspMessagge = 'Parità';
-        document.getElementById('final_result').innerHTML = rspMessagge;
     }
+
+    // Visualizzo esito partita su HTML
+    document.getElementById('final_result').innerHTML = rspMessagge;
+
+    //Funzione per decretare vincitore finale in base al punteggio
+    var finalMex = getFinalResult(userScore, cpuScore, finalMex, 5);
+    document.getElementById('final_message').innerHTML = finalMex;
 
 });
 
-//Condizione finale per decretare vincitore finale in base al punteggio
-if (userScore == 4) {
-    rspMessagge = 'COMPLIMENTI! HAI VINTO LA PARTITA!';
-}   else {
-    rspMessagge = 'Mi dispiace, hai perso la partita. Tenta di nuovo!';
-}   
+// | CARTA
+var paperButton = document.getElementById("paper");
+paperButton.addEventListener("click", function() {
+
+    // Scelta Utente
+    var rspUserChoice = 'carta';
+    document.getElementById('user_choice').innerHTML = rspUserChoice;
+
+    // Assegnazione random segno CPU
+    var rspCpuNumChoice = Math.floor(Math.random() * 3);
+    var rspCpuChoice = rspArr[rspCpuNumChoice];
+    document.getElementById('cpu_choice').innerHTML = rspCpuChoice;
+    
+    // Funzione per stabilire il segno che vince 
+    rspResult = judger(rspUserChoice, rspCpuChoice, rspResult);
+    
+    // Confronto risultato partita con scelta utente
+    if (rspResult == rspUserChoice) {
+        rspMessagge = 'HAI VINTO!';
+        userScore += 1;
+        document.getElementById('score_user').innerHTML = userScore;
+    }   else if (rspResult == rspCpuChoice) {
+        rspMessagge = 'HAI PERSO!';
+        cpuScore += 1;
+        document.getElementById('score_cpu').innerHTML = cpuScore;
+    }   else {
+        rspMessagge = 'Parità';
+    }
+
+    // Visualizzo esito partita su HTML
+    document.getElementById('final_result').innerHTML = rspMessagge;
+
+    //Funzione per decretare vincitore finale in base al punteggio
+    var finalMex = getFinalResult(userScore, cpuScore, finalMex, 5);
+    document.getElementById('final_message').innerHTML = finalMex;
+
+});
+
+// | FORBICE
+var scissorsButton = document.getElementById("scissors");
+scissorsButton.addEventListener("click", function() {
+
+    // Scelta Utente
+    var rspUserChoice = 'forbice';
+    document.getElementById('user_choice').innerHTML = rspUserChoice;
+
+    // Assegnazione random segno CPU
+    var rspCpuNumChoice = Math.floor(Math.random() * 3);
+    var rspCpuChoice = rspArr[rspCpuNumChoice];
+    document.getElementById('cpu_choice').innerHTML = rspCpuChoice;
+    
+    // Funzione per stabilire il segno che vince 
+    rspResult = judger(rspUserChoice, rspCpuChoice, rspResult);
+    
+    // Confronto risultato partita con scelta utente
+    if (rspResult == rspUserChoice) {
+        rspMessagge = 'HAI VINTO!';
+        userScore += 1;
+        document.getElementById('score_user').innerHTML = userScore;
+    }   else if (rspResult == rspCpuChoice) {
+        rspMessagge = 'HAI PERSO!';
+        cpuScore += 1;
+        document.getElementById('score_cpu').innerHTML = cpuScore;
+    }   else {
+        rspMessagge = 'Parità';
+    }
+
+    // Visualizzo esito partita su HTML
+    document.getElementById('final_result').innerHTML = rspMessagge;
+
+    //Funzione per decretare vincitore finale in base al punteggio
+    var finalMex = getFinalResult(userScore, cpuScore, finalMex, 5);
+    document.getElementById('final_message').innerHTML = finalMex;
+
+});
+
+// ! RESET BUTTON
+var resetButton = document.getElementById("game_reset");
+resetButton.addEventListener("click", function() {
+
+    userScore = 0;
+    cpuScore = 0;
+    document.getElementById('user_choice').innerHTML = '-';
+    document.getElementById('cpu_choice').innerHTML = '-';
+    document.getElementById('score_user').innerHTML = '0';
+    document.getElementById('score_cpu').innerHTML = '0';
+    document.getElementById('final_result').innerHTML = '-';
+    document.getElementById('final_message').innerHTML = '';
+
+});
+
+
+
+
 
 // - FUNZIONI -
 
@@ -77,7 +171,21 @@ function judger(v1, v2, res) {
         res = 'Parità!';
     }
 
-    return res
+    return res;
+}
+
+// Funzione per stabilire vincitore finale
+function getFinalResult(score1, score2, message, limit) {
+
+    if (score1 == limit) {
+        message = 'COMPLIMENTI! HAI VINTO LA PARTITA!';
+    }   else if (score2 == limit) {
+        message = 'Mi dispiace, hai perso la partita. Tenta di nuovo!';
+    }   else {
+        message = '';
+    }
+
+    return message;
 }
 
 
