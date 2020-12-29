3a312e14c150b74a0a4e2be70be754b64fbd9179
Cristiano <cristiano.tarconi@gmail.com>
2020-12-29 17:38:36 +0100

    verify prompt validation + small adjs

--- indovina-numero/js/script.js
+++ indovina-numero/js/script.js
@@ -3,35 +3,53 @@ Il giocatore ha a disposizione 4 tentativi. Quando sbaglia gli verrà detto se i
 //|BONUS
 /* provate 3 livelli di complessità */
 
-// Genero numero random da indovinare 
-var starNumber = Math.floor(Math.random() * 99 + 1);
-console.log(starNumber);
-
-// Variabile da inserire manualmente per determinare la difficoltà della partita
-var difficulty = prompt('Digita il livello di difficoltà: easy - medium - hard');
 var game;
-
 //Numero max di tentativi
 var chances = 4;
+// Genero numero random da indovinare 
+var starNumber = Math.floor(Math.random() * 99 + 1);
+/* console.log(starNumber); */
 
+// Assegno livelli di difficoltà ad un array
+var difficultyArr = ['facile', 'medio', 'difficile'];
 
-//Prompt per inserimento numero utente
+// Prompt di inserimento difficoltà con controllo corretta digitazione livello
+do {
+
+    var difficulty = prompt('Digita il livello di difficoltà: facile - medio - difficile');
+    difficulty = difficulty.toLowerCase();
+
+    if (difficultyArr.includes(difficulty) == false) {
+        alert('Nome del livello ERRATO. Prova di nuovo!');
+    } else {
+        alert('Hai selezionato il livello: ' + difficulty.toUpperCase());
+    }
+
+} while (difficultyArr.includes(difficulty) == false);
+
+//Variabile sentinella per while
 var sentinel = true;
 
+// | Prompt inserimento numero Utente con loop while che si interrompe in caso di digitazione numero corretto o in caso i tentativi a disposzione terminassero
 while (chances > 0 && sentinel) {
 
     var userNumber = parseInt(prompt('Indovina il numero, compreso tra 1 e 100'));
+    userNumber = Math.abs(userNumber);
     console.log(userNumber);
 
-    difficultySwitcher();
-
-    console.log('Hai ancora ' + chances + ' possiblità');
+    if (isNaN(userNumber)) {
+        alert('NON HAI DIGITATO ALCUN NUMERO, RIPROVA');
+    } else {
+        difficultySwitcher();
+    }
 }
 
+// Messaggio finale in base all'esito della partita
 var finalMessage = 'COMPLIMENTI! HAI INDOVINATO IL NUMERO!';
 
 if (chances == 0) {
-    finalMessage = 'HAI PERSO!';
+    finalMessage = 'HAI PERSO!' + '\n' + 'Il numero da indovinare era: ' + starNumber;
+    
 }
 
 alert(finalMessage);
@@ -42,19 +60,20 @@ alert(finalMessage);
 function chancesDecreaser() {
     chances--;
     alert('Numero errato!' + '\n' + 'Hai ancora ' + chances + ' possiblità');
+    console.log('Numero errato!' + '\n' + 'Hai ancora ' + chances + ' possiblità');
 }
 
-// Funzione per i livelli di difficoltà
+// | Funzione per i livelli di difficoltà
 function difficultySwitcher() {
 
     switch (difficulty) {
-        case 'easy':
+        case 'facile':
             game = levelEasy(starNumber, userNumber);
             break;
-        case 'medium':
+        case 'medio':
             game = levelMedium(starNumber, userNumber);
             break;
-        case 'hard':
+        case 'difficile':
             game = levelHard(starNumber, userNumber);
             break;
     }
@@ -63,6 +82,7 @@ function difficultySwitcher() {
 }
 
 // Funzioni per differenti livelli di difficoltà
+// Livello facile - Suggerimenti di prossimità al numero su 3 livelli + Numero scelto più alto/basso del numero da indovinare
 function levelEasy(n1, n2) {
 
     var r = Math.abs(n1 - n2);
@@ -91,6 +111,7 @@ function levelEasy(n1, n2) {
 
 }
 
+// Livello medio - Suggerimenti numero scelto più alto/basso del numero da indovinare
 function levelMedium(n1, n2) {
 
     if (n1 == n2) {
@@ -105,6 +126,7 @@ function levelMedium(n1, n2) {
 
 }
 
+// Livello difficile - Nessun suggerimento
 function levelHard(n1, n2) {
 
     if (n1 == n2) {
